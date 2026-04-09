237b918ec165de4b5f5a4e2404551a0130ec0d59
MuratorkunN <orkun.katkat@ozu.edu.tr>
2026-04-09 13:03:38 +0300

    data_creator date update

--- GIS/MGM Risk Indicator/Other/data/data_creator.py
+++ GIS/MGM Risk Indicator/Other/data/data_creator.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 
 SOURCE_FOLDER = "mgm-scrapper-main"
 START_DATE_STR = "2026-01-06"
-END_DATE_STR = "2026-04-01"
+END_DATE_STR = "2026-04-15"
 
 COLUMNS = [
     'date',
